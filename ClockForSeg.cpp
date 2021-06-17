011a631ff1b7e1610099a59979843a49fe07e3f9
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2021-06-18 01:36:21 +0900

    Add: Description

---
+++ ClockForSeg.cpp
@@ -0,0 +1,13 @@
+/*
+ * -ClockForSeg Lib-
+ *
+ * source code file for ClockForSeg Library
+ *
+ * author jihoonkimtech (jihoonkimtech@naver.com)
+ *			(Republic of Korea)
+ * version  V1.0.0
+ * date  2021-06-18
+*/
+
+#include "ClockForSeg.h"
+
